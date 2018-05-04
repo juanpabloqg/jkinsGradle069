384f29d2c311e65f84f1bf040d74d15539d4204c
juanpabloqg <jpqg07@gmail.com>
2018-05-04 08:28:56 -0500

    Commit inicial

---
+++ src/main/java/com/ceiba/jkinsGradle/JkinsGradleApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.jkinsGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkinsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkinsGradleApplication.class, args);
+	}
+}
